2a4debe9bf27b77b93a84c74a34b2e50c5a85d34
Katie Breuil <kcbreuil@gmail.com>
2020-05-30 13:48:28 -0400

    first commit

--- src/components/articles/Article.jsx
+++ src/components/articles/Article.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 
-export const App = () => {
+const Article = () => {
   return <div></div>;
 };
 
-export default App;
+export default Article;
--- src/components/articles/Articles.jsx
+++ src/components/articles/Articles.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 
-export const App = () => {
+const Articles = () => {
   return <div></div>;
 };
 
-export default App;
+export default Articles;
